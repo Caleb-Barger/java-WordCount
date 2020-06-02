ddb2df2714ed9a357e1f8e7473529cae75f8b06d
Caleb <bargercj20@hotmail.com>
2020-06-02 14:48:45 -0700

    hit mvp

--- src/wordcount/Main.java
+++ src/wordcount/Main.java
@@ -1,9 +1,11 @@
 package wordcount;
 
+import java.util.*;
+
 public class Main {
 
     // Easy way to grab some words
-    public String getWords(String words) {
+    public static String getWords(String words) {
         return words;
     }
 
@@ -12,12 +14,42 @@ public class Main {
         return words.replaceAll(regex, "");
     }
     public static void main(String[] args) {
+        // Bind the string
         String myString = getWords(Words.myString);
 
-        System.out.println(myString);
-        String trimmedWords = trimWords(myString, "[[\\.\\?\\!\\,\\;\\:\\{\\}\\(\\)\\']]");
+        String trimmedString = trimWords(myString, "[[\\.\\?\\!\\,\\;\\:\\{\\}\\(\\)\\']]");
 
-        // Show new String
-        System.out.println("\n\n\n\n\n" + trimmedWords);
+        // Split the string into an array
+        String[] words = trimmedString.split(" +");
+
+        // Print out the words
+        // for (String w : words) {
+        //     System.out.println(w);
+        // }
+
+        // Hashmap time!
+        Map<String, Integer> wordsHashMap = new HashMap<>();
+        for (String w : words) {
+            if (!wordsHashMap.containsKey(w)) {
+                wordsHashMap.put(w, 1);
+            } else {
+                int count = wordsHashMap.get(w);
+                wordsHashMap.put(w, count + 1);
+            }
+        }
+
+        // convert into a ArrayList 
+        List<HashMap.Entry> sortedCount = new ArrayList<>(wordsHashMap.entrySet());
+        sortedCount.sort(Comparator.comparing(o -> (int) o.getValue(), Comparator.reverseOrder()));
+
+        // Display Hashmap
+        for (HashMap.Entry e : sortedCount) {
+            System.out.println("\nWord: " + e.getKey() + " appears " + e.getValue() + " times.");
+        }
+
+
+
+
+        
     }
 }
